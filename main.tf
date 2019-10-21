9e27fa92be4ead81571b64ee16b446e43690874d
agosto-lorinronken <lorin.ronken@agosto.com>
2019-10-21 09:30:32 -0500

    Adding real test code

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
-// Adding comments
+#-----------------------#
+# Define Google Provider #
+#-----------------------#
+
 provider "google" {
+}
+
+#----------------------------#
+# Sets Terraform GCS Backend #
+#----------------------------#
+
+terraform {
+  backend "gcs" {
+    bucket      = "tf-demo-bucket"
+    prefix      = "terraform/state"
+  }
+}
+
+resource "google_project" "department_project" {
+  name       = "loronken-test-01"
+  project_id = "loronken-test-02	"
 }
