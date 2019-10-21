a190e274d14f461b2fd09914d912cd894de7fd4b
agosto-lorinronken <lorin.ronken@agosto.com>
2019-10-21 09:40:31 -0500

    Removing bucket for tests

--- main.tf
+++ main.tf
@@ -6,16 +6,6 @@ provider "google" {
   access_token = "${var.access_token}"
 }
 
-#----------------------------#
-# Sets Terraform GCS Backend #
-#----------------------------#
-
-terraform {
-  backend "gcs" {
-    bucket      = "tf-demo-bucket"
-    prefix      = "terraform/state"
-  }
-}
 
 resource "google_project" "department_project" {
   name       = "loronken-test-01"
